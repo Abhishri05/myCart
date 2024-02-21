0a8f6ca912f09399d59f80cf49a79511ff21225f
Abhishek <Abhishri512@gmail.com>
2024-02-21 21:01:27 +0530

    day-2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  final int nums = 30;
+  final String str = "CodePur";
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // HTML Jese Head BOdy aur Foot hota hai iska
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        backgroundColor: Color.fromARGB(255, 240, 200, 250),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Welcome to $nums days of Flutter from $str"),
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
 import "package:flutter/material.dart";
+import "package:my_cart/home_page.dart";
 
-void main(){
-
+void main() {
   runApp(MyApp());
 }
 
-  class MyApp extends StatelessWidget {
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    int nums = 30;
-    String str = "Aditi";
-
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to $nums days of Flutter $str'),
-          )
-          ),
-      ),
+      home: HomePage(),
     );
   }
 }
