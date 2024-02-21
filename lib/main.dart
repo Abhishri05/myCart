f36afc778f80e9bf5750bcfeccf05b52ad8a495d
Abhishek <Abhishri512@gmail.com>
2024-02-21 20:06:09 +0530

    day-2

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import "package:flutter/material.dart";
 
 void main(){
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
   class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    int nums = 30;
+    String str = "Aditi";
+
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: const Text("Welcome to FLutter"),
+            child: Text('Welcome to $nums days of Flutter $str'),
           )
           ),
       ),
