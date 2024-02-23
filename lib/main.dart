66fa7af4d980a4ed545c8ad43b51659b03df49ce
Abhishek <Abhishri512@gmail.com>
2024-02-23 21:06:51 +0530

    day-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:my_cart/Pages/home_page.dart';
+import 'package:my_cart/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+
+      // initialRoute: "/login",
+      routes: {
+        "/": (context) => LoginPage(), //  "/" Route ka Mtlb Home Page
+        "/login": (context) => HomePage()
+      },
     );
   }
 }
-
-bringVegetavble({@required thela, int rupees = 100}) {}
