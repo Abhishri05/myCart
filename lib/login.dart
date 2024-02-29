ffa50ce75847a1e537cc687b3af19d713b0c8547
Abhishek <Abhishri512@gmail.com>
2024-02-29 21:44:26 +0530

    Day -6

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,14 @@
 import "package:flutter/material.dart";
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changebutton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,9 +27,9 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             const SizedBox(
               height: 10,
@@ -33,6 +41,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: const InputDecoration(
                         hintText: "Enter User Name", labelText: "User Name"),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -40,19 +52,53 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Password", labelText: "Password"),
                   ),
                   const SizedBox(height: 40),
-                  ElevatedButton(
-                    onPressed: () {
+
+                  InkWell(
+                    // splashColor: Colors.amber,
+
+                    onTap: () async {
+                      setState(() {
+                        changebutton = true;
+                      });
+
+                      await Future.delayed(const Duration(seconds: 1));
+                      // ignore: use_build_context_synchronously
                       Navigator.pushNamed(context, "/home");
                     },
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.deepPurple,
-                        minimumSize: const Size(100, 40),
-                        shape: const BeveledRectangleBorder()),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white),
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      width: changebutton ? 50 : 120,
+                      height: 40,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          //   shape:
+                          //       changebutton ? BoxShape.circle : BoxShape.rectangle,
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changebutton ? 50 : 8)),
+                      child: changebutton
+                          ? const Icon(Icons.done, color: Colors.amber)
+                          : const Text(
+                              "Login",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
                     ),
-                  )
+                  ),
+
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, "/home");
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //       backgroundColor: Colors.deepPurple,
+                  //       minimumSize: const Size(100, 40),
+                  //       shape: const BeveledRectangleBorder()),
+                  //   child: const Text(
+                  //     "Login",
+                  //     style: TextStyle(color: Colors.white),
+                  //   ),
+                  // )
                 ],
               ),
             )
