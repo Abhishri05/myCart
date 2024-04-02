bbadd49d532ebf8c6927750c876689a9a57eb544
Abhishek <Abhishri512@gmail.com>
2024-04-02 16:37:15 +0530

    Day-16

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -3,7 +3,6 @@ import "package:flutter/services.dart";
 import 'dart:convert';
 import "package:my_cart/models/catalog.dart";
 import "package:my_cart/widgets/drawer.dart";
-import "package:my_cart/widgets/item_widget.dart";
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    const CircularProgressIndicator();
     await Future.delayed(const Duration(seconds: 2));
     var Catajson = await rootBundle.loadString("assets/files/catalog.json");
     var decodedData = jsonDecode(Catajson);
@@ -46,15 +46,46 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          // itemCount: dummylist.length,
+        // child: ListView.builder(
+        //   itemCount: CatalogModel.items.length,
+        //   // itemCount: dummylist.length,
+        //   itemBuilder: (context, index) {
+        //     return ItemWidget(
+        //       item: CatalogModel.items[index],
+        //       // item: dummylist[index],
+        //     );
+        //   },
+        // ),
+
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
           itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-              // item: dummylist[index],
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shadowColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              elevation: 5,
+              child: GridTile(
+                footer: Text("\$${item.price.toString()}"),
+                header: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(color: Colors.deepPurple),
+                  child: Text(
+                    item.name,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                child: Image.network(item.image),
+              ),
             );
           },
+          itemCount: CatalogModel.items.length,
         ),
       ),
 
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: MyTheme.lightTheme(context),
+      theme: MyTheme.lightTheme(context), 
       darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeroute,
       routes: {
